5141fc8aec3b5ea960008afda23c605197d465b6
Matt <m23.cullen@gmail.com>
2021-05-10 17:52:46 -0400

    add stack.navigator and landing stack.screen to app.js

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+
+import LandingScreeen from './components/auth/landing';
 
 const stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-
+      <Stack.Navigator initialRouteName="Landing">
+        <Stack.Screen name="Landing" component={LandingScreeen} options={{ headerShown: false }}/>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
