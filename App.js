8742a875b3015be60fe564e3930797bea8df9016
Matt <m23.cullen@gmail.com>
2021-05-10 18:14:21 -0400

    add comments to App.js

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+// COMPONENT IMPORTS
 import LandingScreeen from './components/auth/Landing';
 
 const Stack = createStackNavigator();
 export default function App() {
   return (
+    // ROUTING WITH REACT NAVIGATION AND REACT NAVIGATION STACK PACKAGES
+    // DOCUMENTATION @https://reactnavigation.org/docs/getting-started AND @https://reactnavigation.org/docs/stack-navigator/
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Landing">
         <Stack.Screen name="Landing" component={LandingScreeen} options={{ headerShown: false }}/>
