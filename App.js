9ca73e8e33a6b121d225740341ca0c152a359725
Matt <m23.cullen@gmail.com>
2021-05-10 17:55:03 -0400

    add react/navigation/stack package and import to App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 
-import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import LandingScreeen from './components/auth/landing';
 
