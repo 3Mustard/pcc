0f6767e3db43e37520f2638092a923f8bb305ce2
Matt <m23.cullen@gmail.com>
2021-05-10 21:00:07 -0400

    add onSignUp method to Register.js

--- components/auth/Register.js
+++ components/auth/Register.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { View, Button, TextInput } from 'react-native';
 
+import firebase from 'firebase';
+
 export class Register extends Component {
     constructor(props){
         super(props);
@@ -12,6 +14,17 @@ export class Register extends Component {
         }
         // Bind function to component
         this.onSignUp = this.onSignUp.bind(this);
+    }
+
+    onSignUp(){
+        const { email, password, username } = this.state;
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then((response) => {
+                console.log(response)
+            })
+            .catch((error) => {
+                console.error(error)
+            })
     }
 
     render() {
