c6f06af7d66df7a63246bb9eab7f8b9bc2e5e3ba
Matt <m23.cullen@gmail.com>
2021-05-10 20:39:41 -0400

    add a View with textInput for user sign up

--- components/auth/Register.js
+++ components/auth/Register.js
@@ -2,11 +2,38 @@ import React, { Component } from 'react'
 import { View, Button, TextInput } from 'react-native';
 
 export class Register extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            email: '',
+            password: '',
+            username: ''
+        }
+    }
+
     render() {
         return (
-            <div>
-                
-            </div>
+            <View>
+                <TextInput 
+                    placeholder="username"
+                    onChangeText={(username) => this.setState({ username })}
+                />
+                <TextInput 
+                    placeholder="email"
+                    onChangeText={(email) => this.setState({ email })}
+                />
+                <TextInput 
+                    placeholder="password"
+                    secureTextEntry={true}
+                    onChangeText={(password) => this.setState({ password })}
+                />
+
+                <Button 
+                    onPress={() => this.onSignUp()}
+                    title="Sign Up"
+                />
+            </View>
         )
     }
 }
